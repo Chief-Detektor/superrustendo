b20feea74653017d4aa0f7288987a5b9e220b60c
Andre Rosenhammer <andre.rosenhammer@gmail.com>
2020-01-23 13:09:11 +0100

    fix coverage script

--- show_coverage.sh
+++ show_coverage.sh
@@ -2,4 +2,4 @@
 #!/usr/bin/env bash
 
 
-cat `ls target/cov/superrustendo-* | head -n3 | tail -n1 | sed 's/.\{1\}$//'`/coverage.json | sed -n -r 's/\s{2}"(percent_covered)": "([0-9]*\.[0-9]*)",/\1: \2/p'
+cat `ls target/cov/decoder-* | head -n3 | tail -n1 | sed 's/.\{1\}$//'`/coverage.json | sed -n -r 's/\s{2}"(percent_covered)": "([0-9]*\.[0-9]*)",/\1: \2/p'
